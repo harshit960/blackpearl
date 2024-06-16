e632df7d704905a6199a2c7350b3da381bda1304
harshit960 <raj.harshit960@gmail.com>
2024-06-16 18:41:34 +0530

    done

---
+++ blkprl/src/App.jsx
@@ -0,0 +1,88 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WebTorrent from 'https://esm.sh/webtorrent'
+import { useEffect } from 'react'
+import { useRef } from 'react'
+import { useSearchParams } from 'react-router-dom';
+
+function App() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const url = formData.get('url');
+    setSearchParams({ url });
+  };
+  const videoRef = useRef(null);
+  const [torrentId, settorrentId] = useState(null);
+
+  useEffect(() => {
+    if (!searchParams.get('url')) {
+      return;
+    }
+    const torrentId = searchParams.get('url');
+    const client = new WebTorrent();
+    // const torrentId = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent';
+
+    const download = () => {
+      client.add(torrentId, torrent => {
+        const file = torrent.files.find(file => file.name.endsWith('.mp4'));
+        file.on('stream', ({ stream, file, req }) => {
+          if (req.destination === 'video') {
+            console.log(`Video player requested data from ${file.name}! Ranges: ${req.headers.range}`);
+            console.log(videoRef.current);
+          }
+        });
+        file.streamTo(videoRef.current);
+        console.log('Ready to play!');
+      });
+    };
+
+    const registerServiceWorker = async () => {
+      try {
+        const reg = await navigator.serviceWorker.register("./sw.min.js", { scope: './' });
+        const worker = reg.active || reg.waiting || reg.installing;
+
+        const checkState = (worker) => {
+          if (worker.state === 'activated') {
+            client.createServer({ controller: reg });
+            download();
+          }
+        };
+
+        if (!checkState(worker)) {
+          worker.addEventListener('statechange', ({ target }) => checkState(target));
+        }
+      } catch (error) {
+        console.error('Service worker registration failed:', error);
+      }
+    };
+
+    registerServiceWorker();
+  });
+  if (!searchParams.get('url')) {
+    return (
+      <div>
+        <h1>Enter a magnet link</h1>
+        <form onSubmit={handleSearch}>
+
+          <input type="text" name="url" onChange={handleSearch} />
+          <button type="submit">Play</button>
+        </form>
+      </div>
+    )
+  }
+  return (
+    <>
+<a className="text-xl" href='/'>
+  Home
+</a>
+      <video ref={videoRef} autoPlay controls style={{ width: '100%' }}></video>
+    </>
+  )
+}
+
+export default App
